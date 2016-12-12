8275802dfcd7ff17fd84c1192f952eb9d36d701f
voelkerb <voelkerb@me.com>
2016-12-12 12:52:43 +0100

    Initial Commit

---
+++ CockYourTail/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CockYourTail
+//
+//  Created by Benjamin Völker on 12/12/2016.
+//  Copyright © 2016 Benjamin Völker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
